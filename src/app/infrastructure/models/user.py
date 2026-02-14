dd4ec418987cccc2479c3f0773bdddaf7e2ed87b
Wertiba <wertibarozen@gmail.com>
2026-02-14 11:52:56 +0300

    fix: user roles table

--- src/app/infrastructure/models/user.py
+++ src/app/infrastructure/models/user.py
@@ -3,9 +3,23 @@ from datetime import datetime, timezone
 
 from pydantic import EmailStr
 from sqlalchemy import Column, DateTime
-from sqlmodel import Field, SQLModel
+from sqlmodel import Field, Relationship, SQLModel
 
-from app.core.schemas.user import UserRole
+
+class UserRole(SQLModel, table=True):
+    __tablename__ = "user_roles"
+
+    user_id: uuid.UUID = Field(foreign_key="users.id", primary_key=True)
+    role_id: uuid.UUID = Field(foreign_key="roles.id", primary_key=True)
+
+
+class Role(SQLModel, table=True):
+    __tablename__ = "roles"
+
+    id: uuid.UUID = Field(default_factory=uuid.uuid4, primary_key=True)
+    value: str
+
+    users: list["User"] = Relationship(back_populates="roles", link_model=UserRole)
 
 
 class User(SQLModel, table=True):
@@ -15,8 +29,6 @@ class User(SQLModel, table=True):
     email: EmailStr = Field(unique=True, nullable=False)
     password: str = Field(nullable=False)
     fullName: str = Field(nullable=False)
-
-    role: UserRole = Field(default=UserRole.VIEWER)
     isActive: bool = Field(default=True)
 
     createdAt: datetime = Field(default_factory=datetime.now)
@@ -24,3 +36,5 @@ class User(SQLModel, table=True):
         default_factory=lambda: datetime.now(timezone.utc),
         sa_column=Column(DateTime(timezone=True), nullable=False),
     )
+
+    roles: list["Role"] = Relationship(back_populates="users", link_model=UserRole)
